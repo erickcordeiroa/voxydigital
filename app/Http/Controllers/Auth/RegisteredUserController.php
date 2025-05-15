857a839abf5381268455e289616f06cd1bfa5b15
Erick Cordeiro <erickcordeiroa@gmail.com>
2025-05-14 21:08:12 -0300

    feat: corrigindo validação dos tenants no cadastro do usuário

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,7 +40,6 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'tenant_id' => 1,
             'password' => Hash::make($request->password),
             'whatsapp' => $request->whatsapp
         ]);
